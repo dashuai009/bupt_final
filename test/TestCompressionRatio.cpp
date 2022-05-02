18ca11e34b296d0051075b118ef8b2f117cf0ee0
dashuai009 <1525836294@qq.com>
2022-05-02 19:06:54 +0800

    improve the test of compression ratio.

--- test/TestCompressionRatio.cpp
+++ test/TestCompressionRatio.cpp
@@ -13,55 +13,89 @@ static const char zipInFiles[5][20] = {
         "data/test_in4.txt"
 };
 static const char config[] = "data/defaultConfig.txt";
-static const char zipOutFile[] = "data/test_out.txt";
+static const char zipOutFiles[5][20] = {
+        "",
+        "data/test_out1.txt",
+        "data/test_out2.txt",
+        "data/test_out3.txt",
+        "data/test_out4.txt"
+};
 static const char unzipOutFile[] = "data/test_unzip.txt";
 
-// zip
-static void ZipTest(benchmark::State &state) {
-    PatternStr::readPatternStr(config);
-    for (auto _: state) {
-        ZIP::compress(zipInFiles[state.range(0)], zipOutFile);
+int64_t fileSize(const char *file_name) {
+    auto input_ptr = fopen(file_name, "rb");
+    if (!input_ptr) {
+        Log(std::cerr, "input_File error!\n");
+        return 0;
     }
-    PatternStr::pattern_str.clear();
+    // obtain file size:
+    fseek(input_ptr, 0, SEEK_END);
+    auto fs = ftell(input_ptr);
+    fclose(input_ptr);
+    return fs;
 }
 
-// unzip
-static void UnZipTest(benchmark::State &state) {
-    PatternStr::readPatternStr(config);
+using F = std::function<void(const char *inputFile, const char *)>;
+auto BM_test = [](benchmark::State &state, const char *inputFile, const char *outputFile, const F &f) {
+    const auto inFileSize = fileSize(inputFile);
+    double zipSpeed = 0;
     for (auto _: state) {
-        UNZIP::uncompress(zipOutFile, unzipOutFile);
+        f(inputFile, outputFile);
+        zipSpeed += inFileSize;
     }
-    PatternStr::pattern_str.clear();
-}
-
-
-//const int iters1 = 100;
-//BENCHMARK(ZipTest)->Iterations(iters1)->Arg(1);
-//BENCHMARK(UnZipTest)->Iterations(iters1)->Arg(1);
-//
-//
-//const int iters2 = 10;
-//BENCHMARK(ZipTest)->Iterations(iters2)->Arg(2);
-//BENCHMARK(UnZipTest)->Iterations(iters2)->Arg(2);
-//
-//
-//const int iters3 = 5;
-//BENCHMARK(ZipTest)->Iterations(iters3)->Arg(3);
-//BENCHMARK(UnZipTest)->Iterations(iters3)->Arg(3);
-
-//
-//const int iters4 = 2;
-//BENCHMARK(ZipTest)->Iterations(iters4)->Arg(3);
-//BENCHMARK(UnZipTest)->Iterations(iters4)->Arg(3);
-
-static int iters[] = {0, 20, 20, 10, 2};
+    const auto outFileSize = fileSize(outputFile);
+    state.counters["ratio"] = std::min(inFileSize, outFileSize) * 1.0 / std::max(inFileSize, outFileSize);
+    state.counters["speed"] = benchmark::Counter(zipSpeed, benchmark::Counter::kIsRate);
+};
+static int iters[] = {0, 100, 20, 10, 5};
+static int max_file_num = 4;
 
 int main(int argc, char **argv) {
     ::benchmark::Initialize(&argc, argv);
     if (::benchmark::ReportUnrecognizedArguments(argc, argv))return 1;
-    for (int i = 1; i <= 4; ++i) {
-        benchmark::RegisterBenchmark("ZipTest", ZipTest)->Iterations(iters[i])->Arg(i);
-        benchmark::RegisterBenchmark("UnZipTest", UnZipTest)->Iterations(iters[i])->Arg(i);
+    PatternStr::readPatternStr(config);
+    char gzipCmd[6][100];
+    char gzipOutFile[6][100];
+
+    char ungzipCmd[6][100];
+    for (int i = 1; i <= max_file_num; ++i) {
+        sprintf(gzipCmd[i], "gzip -k -f %s", zipInFiles[i]);
+        sprintf(gzipOutFile[i], "%s.gz", zipInFiles[i]);
+        sprintf(ungzipCmd[i], "gzip -d -k -f %s.gz", zipInFiles[i]);
+    }
+    for (int i = 1; i <= max_file_num; ++i) {
+        benchmark::RegisterBenchmark("  GzipTest", BM_test, zipInFiles[i], gzipOutFile[i],
+                                     [i, gzipCmd](auto a, auto b) {
+                                         system(gzipCmd[i]);
+                                     })
+                ->Unit(benchmark::kMillisecond)
+                ->Iterations(iters[i])->Arg(i)
+                ->UseRealTime();
+    }
+
+    for (int i = 1; i <= max_file_num; ++i) {
+        benchmark::RegisterBenchmark("   zipTest", BM_test, zipInFiles[i], zipOutFiles[i], ZIP::compress)
+                ->Unit(benchmark::kMillisecond)
+                ->Iterations(iters[i])->Arg(i)
+                ->UseRealTime();
+
+    }
+
+    for (int i = 1; i <= max_file_num; ++i) {
+        benchmark::RegisterBenchmark("UnGzipTest", BM_test, gzipOutFile[i], zipInFiles[i],
+                                     [i, ungzipCmd](auto a, auto b) {
+                                         system(ungzipCmd[i]);
+                                     })
+                ->Unit(benchmark::kMillisecond)
+                ->Iterations(iters[i])->Arg(i)
+                ->UseRealTime();
+
+    }
+    for (int i = 1; i <= max_file_num; ++i) {
+        benchmark::RegisterBenchmark(" unzipTest", BM_test, zipOutFiles[i], unzipOutFile, UNZIP::uncompress)
+                ->Unit(benchmark::kMillisecond)
+                ->Iterations(iters[i])->Arg(i)
+                ->UseRealTime();
     }
     ::benchmark::RunSpecifiedBenchmarks();
     ::benchmark::Shutdown();
